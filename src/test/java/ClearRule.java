0f3024ef5118fd53bf5572996f5c7add6a27ceb0
Polina Nenchev <polina.mihova@yahoo.com>
2016-02-25 14:48:56 -0800

    Create initial files

---
+++ src/test/java/ClearRule.java
@@ -0,0 +1,15 @@
+//ONLY TO BE USED IF STORING SESSION DATA IN SPARK
+
+// import org.junit.rules.ExternalResource;
+//
+// public class ClearRule extends ExternalResource {
+//
+//   protected void before() {
+//    }
+//
+//   protected void after() {
+//     App.clear();
+//     Client.clear();
+//     Stylist.clear();
+//   }
+// }
