1294dc03a2541caa88aa30553d53042d6fb26a48
Polina Nenchev <polina.mihova@yahoo.com>
2016-02-27 13:53:03 -0800

    Add integration tests for creatingn stylist, edit and delete stylist

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest extends FluentTest {
   @Test
   public void rootTest() {
     goTo("http://localhost:4567/");
-    assertThat(pageSource()).contains("");
+    assertThat(pageSource()).contains("Hair Salon");
   }
 
   @Test
@@ -34,5 +34,53 @@ public class AppTest extends FluentTest {
     assertThat(pageSource()).contains("Jennie Thompson");
   }
 
-  
+  @Test
+  public void displaysAllClientsOnceAdded() {
+    Stylist newStylist = new Stylist("Jennie Thompson");
+    newStylist.save();
+    Client firstClient = new Client ("Linda", newStylist.getId());
+    firstClient.save();
+    String categoryPath = String.format("http://localhost:4567/stylist/%d", newStylist.getId());
+    goTo(categoryPath);
+    assertThat(pageSource()).contains("Linda");
+  }
+
+  @Test
+  public void update_updateStylistNameInDatabase() {
+    Stylist newStylist = new Stylist("Jennie Thompson");
+    newStylist.save();
+    newStylist.update("Polina Nenchev");
+    String categoryPath = String.format("http://localhost:4567/stylist/%d", newStylist.getId());
+    goTo(categoryPath);
+    assertThat(pageSource()).contains("Polina Nenchev");
+  }
+
+  @Test
+  public void delete_deletesStylistsNameFromDatabase() {
+    Stylist newStylist = new Stylist("Jennie Thompson");
+    newStylist.save();
+    newStylist.delete();
+    String categoryPath = String.format("http://localhost:4567/", newStylist.getId());
+    goTo(categoryPath);
+    assertThat(!(pageSource()).contains("Jennie Thompson"));
+  }
+
+  // @Test
+  // public void addsClientsToStylistPage() {
+  //   Stylist newStylist = new Stylist("Jennie Thompson");
+  //   newStylist.save();
+  //
+  // }
+  //
+  // @Test
+  // public void stylistIsUpdate() {
+  //   Stylist stylist = new Stylist("Jennie Thompson");
+  //   stylist.save();
+  //   stylist.update("Polina Nenchev");
+  //   String categoryPath = String.format("http://localhost://4567", stylist.getId());
+  //   goTo(categoryPath);
+  //   assertThat(pageSource()).contains("Polina Nenchev");
+  // }
+
+
 }
